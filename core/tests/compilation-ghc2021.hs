126e59d171de6462154401b84468071b67904b34
Greg Pfeil <greg@technomadic.org>
2026-02-04 15:16:23 -0700

    Fix formatting issues

--- core/tests/compilation-ghc2021.hs
+++ core/tests/compilation-ghc2021.hs
@@ -8,14 +8,6 @@ import Data.Monoid ((<>))
 import System.IO (IO)
 import qualified Test.SupportGHC2021
 import Prelude ()
-#if MIN_VERSION_GLASGOW_HASKELL(9, 10, 1, 0)
-import qualified Test.SupportGHC2024
-#endif
 
 main :: IO ()
-main =
-  pure $
-    Test.SupportGHC2021.force
-#if MIN_VERSION_GLASGOW_HASKELL(9, 10, 1, 0)
-      <> Test.SupportGHC2024.force
-#endif
+main = pure Test.SupportGHC2021.force
